7d6b76bfd2203c6e26ccc0628a3e66f2298af5b3
briskgreen <briskgreen@163.com>
2013-09-22 02:13:38 +0800

    更新

--- titlebot.py
+++ titlebot.py
@@ -37,21 +37,23 @@ def pong_serv(ssl,str):
 	ssl.send(''.join(l));
 
 def get_title(ssl,p,chan):
-	try:
-		get=urllib2.urlopen(p[0]).read();
-	except Exception,err:
-		ssl.send("PRIVMSG "+chan+" :"+str(err)+"\n");
-		return;
-	temp=re.compile('.*<title>(.*)</title>').findall(get);
-	if temp:
-		ssl.send("PRIVMSG "+chan+" :"+"标题："+_decode(temp[0])+"\n");
-	else:
-		ssl.send("PRIVMSG "+chan+" :啊哦，淫家木有发现标题了啦@.@\n");
+	for url in p:
+		try:
+			get=urllib2.urlopen(url).read();
+		except Exception,err:
+			ssl.send("PRIVMSG "+chan+" :"+str(err)+"\n");
+			continue;
+		temp=re.compile('<title>(.[^<]*)').findall(get);
+		if temp:
+			ssl.send("PRIVMSG "+chan+" :"+"标题："+_decode(temp[0])+"\n");
+		else:
+			ssl.send("PRIVMSG "+chan+" :啊哦，淫家木有发现标题了啦@.@\n");
 
 def get_magnet(ssl,p,chan):
-	s1=urllib2.unquote(p[0]).replace("\n","");
-	s2=s1.replace("\r","");
-	ssl.send("PRIVMSG "+chan+" :"+"标题："+s2+"\n");
+	for magnet in p:
+		s1=urllib2.unquote(magnet.replace("\n",""));
+		s2=s1.replace("\r","");
+		ssl.send("PRIVMSG "+chan+" :"+"标题："+s2+"\n");
 
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
 s.connect(("irc.freenode.net",7000));
@@ -64,11 +66,11 @@ join_channel(ssl);
 while True:
     data=ssl.recv(1024);
     if 'http' and 'PRIVMSG' in data:
-        p=re.compile('.*(http.?://[\S]*)').findall(data);
+        p=re.compile('(http.?://[\S]*)').findall(data);
         if p:
 		get_title(ssl,p,get_channel(data));
 		del(p);
-    p=re.compile('.*magnet:\?xt=urn:btih:.[^&]*&amp;dn=(.[\S]*)').findall(data);
+    p=re.compile('magnet:\?xt=urn:btih:.[^&]*&amp;dn=(.[\S]*)').findall(data);
     if p:
 	    get_magnet(ssl,p,get_channel(data));
     if 'PING' in data and 'PRIVMSG' not in data:
